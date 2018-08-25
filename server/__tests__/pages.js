cd332cc561d17b2e611d2f4e03240a06305fede0
mkamakura <kamakura.masaya@gmail.com>
2018-08-26 01:47:26 +0900

    tmp

--- server/__tests__/pages.js
+++
@@ -1,9 +0,0 @@
-import assert from 'assert'
-import rewire from 'rewire'
-
-const pages = rewire('../pages.js')
-const isToGetHtmlFile = pages.__get__('isToGetHtmlFile')
-
-test('pages: isToGetHtmlFile', () => {
-
-})
