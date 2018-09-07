e085599fe73c0007a6fd5a8cd71274a2f2ea1f64
mkamakura <kamakura.masaya@gmail.com>
2018-09-07 13:44:18 +0900

    feat(styles): add

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,11 @@ import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardContent from '@material-ui/core/CardContent'
 import Icon from '@material-ui/core/Icon'
+import Switch from '@material-ui/core/Switch'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import { withStyles } from '@material-ui/core/styles';
+
 import configure from '../mocks/configure' // TODO
 
 export default () => {
@@ -17,11 +22,18 @@ export default () => {
       <AppBar position="static" color="primary">
         <Toolbar>houseplant</Toolbar>
       </AppBar>
-      <Typography variant="headline" gutterBottom>
-        HTML
-      </Typography>
-      {<SiteSelect siteGroup={siteGroup} />}
-      {<Pages pages={pages} />}
+      <main>
+        <Typography variant="headline" gutterBottom>
+          HTML
+        </Typography>
+        {<SiteSelect siteGroup={siteGroup}/>}
+        {<Pages pages={pages}/>}
+      </main>
+      <style jsx>{`
+        main {
+          padding: 20px;
+        }
+      `}</style>
     </div>
   )
 }
@@ -29,23 +41,34 @@ export default () => {
 function SiteSelect({ siteGroup }) {
   return (
     <div>
-      {Object.entries(siteGroup).map(([key, val]) => (
-        <div>
-          {key}
-          {val}
-        </div>
-      ))}
+      <FormGroup row>
+        {Object.entries(siteGroup).map(([key, val]) => (
+          <FormControlLabel
+            control={
+              <Switch
+                checked={true}
+                onChange={() => {
+                }}
+                value={key}
+              />
+            }
+            label={val}
+          />
+        ))}
+      </FormGroup>
     </div>
   )
 }
+
+const MyCard = withStyles({ root: { 'margin-bottom': 20 }})(Card);
 
 function Pages({ pages }) {
   return (
     <div>
       {pages.map(page => {
         return (
-          <Card>
-            <CardHeader title={page.description} subheader={page.uri} />
+          <MyCard>
+            <CardHeader title={page.description} subheader={page.uri}/>
             <CardContent>
               {isArray(page.variations)
                 ? (() => {
@@ -53,7 +76,7 @@ function Pages({ pages }) {
                   return variations.map(variation => {
                     const uri = URI(`http://localhost:8180/${page.uri}/${variation.baseFile}`)
                       .query({
-                        houseplant: variation.label
+                        houseplant: variation.label,
                       })
                       .toString()
                     return (
@@ -66,29 +89,29 @@ function Pages({ pages }) {
                 })()
                 : Object.keys(page.variations).map(key => {
                   const variations = page.variations[key]
-                    return variations.map(variation => {
-                      const { label, description } = variation
-                      const uri = URI(`http://localhost:8180/${page.uri}/${variation.baseFile}`)
-                        .query({
-                          houseplant: label,
-                          device: key
-                        })
-                        .toString()
-                      return (
+                  return variations.map(variation => {
+                    const { label, description } = variation
+                    const uri = URI(`http://localhost:8180/${page.uri}/${variation.baseFile}`)
+                      .query({
+                        houseplant: label,
+                        device: key,
+                      })
+                      .toString()
+                    return (
+                      <div>
                         <div>
-                          <div>
                           <Icon>
                             {key === 'pc' ? 'desktop_windows' : 'smartphone'}
                           </Icon>
                           <span>{description}</span>
-                          </div>
-                          <a href={uri}>{uri}</a>
                         </div>
-                      )
-                    })
-                  })}
+                        <a href={uri}>{uri}</a>
+                      </div>
+                    )
+                  })
+                })}
             </CardContent>
-          </Card>
+          </MyCard>
         )
       })}
     </div>
