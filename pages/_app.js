ef8dad939a85e49bc18174a60a0d9b09b6001f26
mkamakura <kamakura.masaya@gmail.com>
2018-09-07 13:15:27 +0900

    chore(material-ui, ssr)

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,45 @@
-import App, {Container} from 'next/app'
+import App, { Container } from 'next/app'
 import React from 'react'
-import withReduxStore from '../lib/with-redux-store'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import { Provider } from 'react-redux'
+import withReduxStore from '../lib/with-redux-store'
+import getPageContext from '../lib/getPageContext'
+import JssProvider from 'react-jss/lib/JssProvider'
 
 class MyApp extends App {
-  render () {
-    const {Component, pageProps, reduxStore} = this.props
+  constructor(props) {
+    super(props)
+    this.pageContext = getPageContext()
+  }
+
+  pageContext = null
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }
+
+
+  render() {
+    const { Component, pageProps, reduxStore } = this.props
+    console.log(this.pageContext)
     return (
       <Container>
         <Provider store={reduxStore}>
-          <Component {...pageProps} />
+          <JssProvider
+            registry={this.pageContext.sheetsRegistry}
+            generateClassName={this.pageContext.generateClassName}>
+            <MuiThemeProvider
+              theme={this.pageContext.theme}
+              sheetsManager={this.pageContext.sheetsManager}>
+              <CssBaseline/>
+              <Component pageContext={this.pageContext} {...pageProps} />
+            </MuiThemeProvider>
+          </JssProvider>
         </Provider>
       </Container>
     )
